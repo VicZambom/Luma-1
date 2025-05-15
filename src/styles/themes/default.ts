d32621988687b5b9979b0d2296b94d6c334ff5d7
VicZambom <VictoriaG.ZambomP@gmail.com>
2025-05-15 17:23:14 -0300

    atualizando

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,12 @@
+export const defaultTheme ={
+    'purple-100': '#5D3998',
+    'purple-300': '#6945A4',
+    'purle-500': '#9271CA',
+    'purple-700': '#8B67C6',
+    
+    
+    'gray-100': '#C3C3C3',
+    'white': '#FFFFFF',
+    'black': '000000'
+    
+}as const
